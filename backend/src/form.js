3f9c93b104ce6d48a1f5165507fbe81dfc528d02
tran <tran@silpion.de>
2016-06-10 14:59:23 +0200

    optimize date input

--- backend/src/form.js
+++ backend/src/form.js
@@ -109,11 +109,11 @@ module.exports = cms => {
             return merge(defaultOptions.form, {type: 'input', templateOptions: {type: 'number'}}, field.form);
         }
         if (field.type === Date) {
-            return merge(defaultOptions.form, {type: 'input', templateOptions: {type: 'date'}}, field.form);
+            return merge(defaultOptions.form, {type: 'input', templateOptions: {type: 'datetime'}}, field.form);
         }
         if (field === String) return merge(defaultOptions.form, {type: 'input'});
         if (field === Boolean) return merge(defaultOptions.form, {type: 'checkbox'});
-        if (field === Date) return merge(defaultOptions.form, {type: 'input', templateOptions: {type: 'date'}});
+        if (field === Date) return merge(defaultOptions.form, {type: 'input', templateOptions: {type: 'datetime'}});
         if (field === Number) return merge(defaultOptions.form, {type: 'input', templateOptions: {type: 'number'}});
 
     })
