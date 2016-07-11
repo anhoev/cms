e3b3be15ec739c4fb12016c5387fae71be89297d
tran <tran@silpion.de>
2016-07-11 11:46:10 +0200

    update mongoose fix populate for formly

--- frontend/components/formly/ref.controller.js
+++ frontend/components/formly/ref.controller.js
@@ -10,9 +10,12 @@ function controller($scope, cms) {
 
         if (typeof $scope.model[$scope.options.key] === 'string') {
             $scope.model[$scope.options.key] = _.find($scope.models, {_id: $scope.model[$scope.options.key]});
+        } else if (Array.isArray($scope.model[$scope.options.key]) && typeof $scope.model[$scope.options.key][0] === 'string') {
+            const _ids = JsonFn.clone($scope.model[$scope.options.key]);
+            $scope.model[$scope.options.key] = _.filter($scope.models, ({_id}) => _ids.indexOf(_id) !== -1);
         }
     })
-    
+
 }
 
 export default controller;
