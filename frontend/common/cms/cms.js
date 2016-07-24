97f0f75c97438e7b61c637411013cfee9076120a
tran <tran@silpion.de>
2016-07-24 22:17:45 +0200

    fix: uri

--- frontend/common/cms/cms.js
+++ frontend/common/cms/cms.js
@@ -424,7 +424,16 @@ function run(cms, $http, $websocket) {
     } catch (e) {
     }
 
-    window.socket = cms.socket = $websocket('ws://localhost:8888');
+    let loc = window.location, new_uri;
+    if (loc.protocol === "https:") {
+        new_uri = "wss:";
+    } else {
+        new_uri = "ws:";
+    }
+    new_uri += "//" + loc.host;
+    new_uri += loc.pathname;
+
+    window.socket = cms.socket = $websocket(new_uri);
 }
 
 export default modelModule.name;
