f094a5128bae3d995cf38a83b1db1603cda04b7e
Anh <anhoev@yahoo.de>
2019-01-02 11:08:21 +0700

    delete unnecessary files

--- backend/mobile/index.js
+++ backend/mobile/index.js
@@ -4,20 +4,10 @@
 
 'use strict';
 const _ = require('lodash');
-const autopopulate = require('mongoose-autopopulate');
 const cms = require('../src/cms');
-const resolvePath = cms.resolvePath = (p) => `backend/mobile/${p}`;
 cms.data.security = false;
 cms.listen(8888);
 cms.useSession();
 cms.mongoose.connect('mongodb://localhost/mobile10');
 
 cms.use(require('./test'));
-
-//cms.server('backend/mobile/en', '');
-
-cms.menu = {
-    bodyPaddingTop: '120px'
-}
-
-cms.data.online.wsAddress = 'ws://localhost:8888';
