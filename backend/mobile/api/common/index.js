7106615065c3812310fdca63e47852a7b58211fa
Anh <anhoev@yahoo.de>
2019-11-07 11:06:58 +0700

    remove express api

--- backend/mobile/api/common/index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commonCtrl = require('./common.controller');
-
-module.exports = router;
--- backend/mobile/api/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-module.exports = function(app) {
-  app.use('/api/v1', router);
-};
