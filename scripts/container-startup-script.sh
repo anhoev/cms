85a60e3f35724ea76906cb9390a32885e00507c4
Lam Phan Dang <lam.phan@gigasouce.io>
2019-11-15 17:15:44 +0700

    test

--- scripts/container-startup-script.sh
+++ scripts/container-startup-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
-GITBOT_ACCESS_TOKEN=$(cat ./gitbot-access-token)
-rm ./gitbot-access-token
+GITBOT_ACCESS_TOKEN=$(cat ./gitbot)
+rm ./gitbot
 git config --global url."https://${GITBOT_ACCESS_TOKEN}:@github.com/".insteadOf "https://github.com/"
 node backend/use/index.js --config=./config/config.json --mode=safemode
