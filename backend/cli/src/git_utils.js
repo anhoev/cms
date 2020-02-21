755dee421a0a053a7c7bb8df8c56845e22a19a93
transse04731 <triracle@gmail.com>
2020-02-21 17:05:44 +0700

    add update submodule in cli

--- backend/cli/src/git_utils.js
+++ backend/cli/src/git_utils.js
@@ -1,5 +1,26 @@
 const { spawnSync } = require('child_process');
 
+function updateSubmodule() {
+  const result = spawnSync('git', ['submodule', 'update', '--init', '--recursive'], {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: 'pipe',
+    encoding: 'utf-8',
+    silient: true
+  });
+  return result.stdout;
+}
+
+module.exports.initGit = function (path) {
+  const result = spawnSync('git', ['init'], {
+    cwd: path,
+    env: process.env,
+    stdio: 'pipe',
+    encoding: 'utf-8',
+    silient: true
+  })
+};
+
 module.exports.addSubmodule = function (url) {
   const result = spawnSync('git', ['submodule', 'add', url], {
     cwd: process.cwd(),
@@ -8,6 +29,9 @@ module.exports.addSubmodule = function (url) {
     encoding: 'utf-8',
     silient: true
   });
+  if (result.status === 1) {
+    return updateSubmodule();
+  }
   return result.stdout;
 };
 
