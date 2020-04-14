49c48689c59f4df3db5181c4d02ac83eb286528f
anhoev <anhoev@yahoo.de>
2020-04-14 17:57:48 +0700

    fix serve.sh

--- backend/use/serve.sh
+++ backend/use/serve.sh
@@ -1,4 +1,6 @@
 echo -en "\\033]0;Backoffice\\a"
 THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
 cd $THIS_DIR/../../../backoffice
-npm run serve
+PORT=`cat ../.frontend-lock`
+echo $PORT
+npx vue-cli-service serve --port=$PORT
