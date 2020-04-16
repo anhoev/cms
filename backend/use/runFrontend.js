c634fc3e861ce28fe76b553e58553c7aa4676178
transse04731 <triracle@gmail.com>
2020-04-16 16:01:37 +0700

    run parallel project

--- backend/use/runFrontend.js
+++ backend/use/runFrontend.js
@@ -1,4 +1,4 @@
-const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 const path = require('path');
 const portfinder = require('portfinder');
 const fs = require('fs');
@@ -8,24 +8,28 @@ module.exports = async function () {
   if (fs.existsSync(path.join(__dirname, '../../../dist'))) {
     return;
   }
-  const lockPath = path.resolve(__dirname, '../../../.frontend-lock');
+  const lockPath = path.resolve(__dirname, '../../../.frontend-lock.json');
+  let config = {};
+  if (fs.existsSync(lockPath)) {
+    config = require(lockPath);
+  }
   const run = async function () {
     const port = await portfinder.getPortPromise({
       port: 8080,
     })
-    fs.writeFileSync(lockPath, port, 'utf-8');
-    const child = spawn('open', ['-a', 'Terminal', `serve.sh`], {stdio: 'inherit', cwd: __dirname});
+    config[global.APP_CONFIG.pluginPath] = port;
+    fs.writeFileSync(lockPath, JSON.stringify(config), 'utf-8');
+    const child = exec(`osascript -e 'tell application "Terminal" to do script "cd ${path.resolve(__dirname, '../../../backoffice')} && npx vue-cli-service serve --port=${port} --pluginPath=${global.APP_CONFIG.pluginPath}"'`, {stdio: 'inherit'});
     global.APP_CONFIG.proxyPort = port;
   }
   if (!fs.existsSync(lockPath)) {
     await run();
   } else {
-    const port = parseInt(fs.readFileSync(lockPath, 'utf-8'));
-    const inUse = await tcpPortUsed.check(port, '127.0.0.1');
+    const port = require(lockPath)[global.APP_CONFIG.pluginPath];
+    const inUse = port ? await tcpPortUsed.check(port, '127.0.0.1') : false;
     if (inUse) {
       global.APP_CONFIG.proxyPort = port;
     } else {
-      fs.unlinkSync(lockPath);
       await run();
     }
   }
