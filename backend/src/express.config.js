cee63ef9a83909256cb82abc989ff5c105a19499
danglam105 <danglam105@gmail.com>
2020-04-17 10:11:18 +0700

    Allow iframe in production if allowIframe config is set to true (#86)

--- backend/src/express.config.js
+++ backend/src/express.config.js
@@ -53,6 +53,13 @@ module.exports = function (cms, config = {}) {
 
   cms.post('load:types', () => {
     if (fs.existsSync(path.join(__dirname, '../../../dist'))) {
+      if (global.APP_CONFIG.allowIframe) {
+        cms.app.use(function (req, res, next) {
+          res.setHeader('Content-Security-Policy', 'frame-ancestors *')
+          next()
+        })
+      }
+
       cms.r2.use('/', history(), cms.middleware.getTypesMiddleware, cms.express.static(path.join(__dirname, '../../../dist')));
     } else {
       const backofficeProxy = proxy('/', {
