75066eb7d7b0a9b642f2b0af606bf9b74f030955
ThinhVu <vutrongthinhk7@gmail.com>
2020-04-20 10:50:24 +0700

    Add delete file by path api

--- backend/src/file-explorer-backend/index.js
+++ backend/src/file-explorer-backend/index.js
@@ -15,6 +15,7 @@ const gridFsFileStorage = new GridFsFileStorage(mongoose.connection.db, {
 module.exports = function (cms) {
   const {
     deleteFile,
+    deleteFileByFilePath,
     getFileMetadata,
     createFolder,
     listFilesByFolder,
@@ -40,13 +41,16 @@ module.exports = function (cms) {
   route.get('/folder-tree', getFolderTree);
   route.get('/property-mappings', getPropertyMappings);
   route.post('/folders', createFolder);
-  route.delete('/files/:id(*)', deleteFile);
+  route.delete('/files/:id', deleteFile);
   route.put('/file-metadata/rename/:id', renameFileMetadata);
   route.put('/file-metadata/move/:id', moveFileMetadata);
   // APIs if GridFS driver is used
   route.get('/files/view/:filePath(*)', viewFileByFilePath);
   route.get('/files/download/:filePath(*)', downloadFileByFilePath);
   route.post('/files', uploadFile);
+
+  // file path's api
+  route.delete('/filePaths/:filePath(*)', deleteFileByFilePath);
 
   cms.app.use('/cms-files', route);
 };
