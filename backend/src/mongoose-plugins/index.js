31f4bea9a64315a8e642889c53cff8e42db840f5
danglam105 <danglam105@gmail.com>
2020-05-15 18:39:04 +0700

    Move write concern to config file

--- backend/src/mongoose-plugins/index.js
+++ backend/src/mongoose-plugins/index.js
@@ -2,7 +2,7 @@ module.exports = function () {
   const mongoose = require('mongoose');
 
   const writeConcernMajorityPlugin = function (schema) {
-    schema.set('writeConcern', {w: 'majority'});
+    schema.set('writeConcern', {w: global.APP_CONFIG.writeConcern});
   }
 
   mongoose.plugin(writeConcernMajorityPlugin);
