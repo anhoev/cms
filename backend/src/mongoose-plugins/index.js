20df01af7d8364e42badb1218118112e9ade96cc
danglam105 <danglam105@gmail.com>
2020-05-25 18:58:59 +0700

    Revert "Add default write concern if write concern config is not set"
    This reverts commit fd88fe05

--- backend/src/mongoose-plugins/index.js
+++ backend/src/mongoose-plugins/index.js
@@ -1,9 +1,9 @@
 module.exports = function () {
   const mongoose = require('mongoose');
 
-  const writeConcernPlugin = function (schema) {
-    schema.set('writeConcern', {w: global.APP_CONFIG.writeConcern || 'primary'});
+  const writeConcernMajorityPlugin = function (schema) {
+    schema.set('writeConcern', {w: global.APP_CONFIG.writeConcern});
   }
 
-  mongoose.plugin(writeConcernPlugin);
+  mongoose.plugin(writeConcernMajorityPlugin);
 }
